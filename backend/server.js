1a14a43f280c242f9b8d9c653e87e171d5cfe041
Sahil <114127576+sahilghewari@users.noreply.github.com>
2024-10-05 15:45:48 +0530

    90% project completed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,14 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors()); 
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials: true
+  }
+)); 
+
 app.use(express.json());  
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
